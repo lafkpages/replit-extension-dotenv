074171b4f3bca10239609ebef538a59e3c186ed3
lafkpages <soy.lafk+gh@gmail.com>
2023-09-11 16:13:31 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+  import Button from '@replit-svelte/ui/Button.svelte';
+  import Plus from '@replit-svelte/ui/icons/Plus.svelte';
+  import { showModal, showToast } from '@replit-svelte/utils';
+
+  import { messages } from '@replit/extensions';
+</script>
+
+<svelte:head>
+  <title>Replit UI Svelte Extension</title>
+</svelte:head>
+
+<h1 class="headerBig">Replit UI Svelte Extension</h1>
+
+<Button
+  text="My button"
+  on:click={() => {
+    showModal({
+      title: 'Button clicked',
+      text: 'This is a demo Modal text',
+    });
+  }}
+/>
+
+<Button
+  variant="primary"
+  on:click={async () => {
+    // Toast inside this pane
+    showToast({
+      text: 'Example toast 1',
+      variant: 'primary',
+    });
+
+    // Toast outside this pane
+    await messages.showNotice('Example toast 2');
+  }}
+>
+  <Plus />
+  Show toast
+</Button>
---
+++ src/routes/fileHandler/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Input from '@replit-svelte/ui/Input.svelte';
+
+  // File handler file path
+  import { fileHandlerPath } from '$lib/stores';
+</script>
+
+<svelte:head>
+  <title>File handler demo - Replit UI Svelte Extension</title>
+</svelte:head>
+
+This is a file handler.
+
+{#if $fileHandlerPath}
+  You selected the file:
+  <Input value={$fileHandlerPath} disabled />
+{:else}
+  You haven't selected a file.
+{/if}
