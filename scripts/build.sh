de10f91d18e1ccb2b02c17bb8247f5eb788b76c9
lafkpages <soy.lafk+gh@gmail.com>
2023-09-11 17:23:39 +0000

    Build script pipefail for real build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,9 @@
 "$REPL_HOME/scripts/disableSvelteServer.sh"
 
 # Run the SvelteKit build adapter
+set -o pipefail
 pnpm run build
+set +o pipefail
 
 # Reenable LSPs
 "$REPL_HOME/scripts/enableSvelteServer.sh"
