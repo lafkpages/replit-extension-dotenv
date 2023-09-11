f18aab43648ca74d086c14bbeff8d616af7d7523
lafkpages <soy.lafk+gh@gmail.com>
2023-09-11 16:35:02 +0000

    .env loading state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <script>
   import { onMount } from 'svelte';
 
+  import Loader from '@replit-svelte/ui/icons/Loader.svelte';
+
   import { showToast } from '@replit-svelte/utils';
 
   import { readFile } from '@replit/extensions';
 
   let value = '';
+  let loadingSecrets = true;
 
   onMount(async () => {
     const data = await readFile('.env', 'utf8');
@@ -21,6 +24,7 @@
     }
 
     value = data.content;
+    loadingSecrets = false;
   });
 </script>
 
@@ -30,10 +34,40 @@
 
 <h1 class="headerBig">.env</h1>
 
-<textarea bind:value></textarea>
+<div class="editor-wrapper">
+  {#if loadingSecrets}
+    <div class="loading-secrets">
+      <Loader />
+      <h2 class="subheadDefault">Loading secrets</h2>
+    </div>
+  {/if}
+  <textarea bind:value disabled={loadingSecrets}></textarea>
+</div>
 
 <style>
-  textarea {
+  .editor-wrapper {
+    width: 100%;
     height: 100%;
+    position: relative;
+  }
+
+  .loading-secrets {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: var(--space-4);
+    background-color: var(--background-overlay);
+  }
+  
+  textarea {
+    width: 100%;
+    height: 100%;
+    resize: none;
   }
 </style>
